b1b33e25fb933282f1ab847462826a0ab727cea2
LegoMan28 <craftersofbedrock@gmail.com>
2013-07-26 07:49:24 -0400

    HelloWorld! Code

---
+++ HelloWorld!.java
@@ -0,0 +1,13 @@
+/** Comment
+ * Displays "Hello World!" to the standard output.
+ */
+
+class HelloWorld {
+
+  public static void main (String args[]) {
+
+    System.out.println("Hello World!");   //Displays the enclosed String on the Screen Console
+
+  }
+  
+}
